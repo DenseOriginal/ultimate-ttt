7fd253a3e963cbcaa946629f1563b3a66c56f87b
Anders Kok <ak.kornerup@gmail.com>
2021-05-18 16:27:47 +0200

    Added content to board component

--- src/app/game/components/board/board.component.ts
+++ src/app/game/components/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-board',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
+
+  @Input() active!: boolean;
 
   constructor() { }
 
