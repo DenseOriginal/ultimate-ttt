380b66a208457a232cb840cd3935543ed7f40cae
Anders Kok <ak.kornerup@gmail.com>
2021-05-18 20:48:32 +0200

    Display some information

--- src/app/game/page/game.component.ts
+++ src/app/game/page/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from 'src/app/services/game.service';
 
 @Component({
   selector: 'app-game',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+  constructor(public gameService: GameService) { }
 
   ngOnInit(): void {
   }
