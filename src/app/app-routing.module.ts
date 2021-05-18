199bd13217c30a30f39b73aa20f3bf27e520f3fd
Anders Kok <ak.kornerup@gmail.com>
2021-05-18 12:11:42 +0200

    Created game route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'game', loadChildren: () => import('./game/game.module').then(m => m.GameModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
