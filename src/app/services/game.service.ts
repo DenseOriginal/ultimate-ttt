e6cd4479942f6968af77e54e00b0426dc62a0ace
Anders Kok <ak.kornerup@gmail.com>
2021-05-18 16:40:28 +0200

    Wrote code for gameService

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
+type Player = "red" | "blue";
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
+  private _state = new BehaviorSubject('-'.repeat(81));
+  public state = this._state.asObservable();
+
   constructor() { }
+
+  place(board: number, field: number, player: Player): void {
+    let newState = this._state.value.split('');
+    newState[board * 9 + field] = player == 'red' ? 'R' : 'B';
+    this._state.next(newState.join(''));
+  }
 }
