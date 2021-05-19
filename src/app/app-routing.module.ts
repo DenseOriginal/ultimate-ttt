4173bc00b9ff8b5e6855d61ebd55a60e3ed7743f
Anders Kok <ak.kornerup@gmail.com>
2021-05-19 21:03:58 +0200

    Generated home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'game', loadChildren: () => import('./game/game.module').then(m => m.GameModule) }
+  { path: 'game', loadChildren: () => import('./game/game.module').then(m => m.GameModule) },
+  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }
 ];
 
 @NgModule({
