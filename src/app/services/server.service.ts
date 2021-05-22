f0cdd812244e3d9e88488fc316ee7ec56f308046
Anders Kok <ak.kornerup@gmail.com>
2021-05-22 21:28:07 +0200

    Wrote stuff in server service

--- src/app/services/server.service.ts
+++ src/app/services/server.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
 
+interface GameDoc {
+  red: string;
+  blue: string;
+  state: string;
+  lastmove: string;
+}
+
+interface HistoryDoc {
+  board: number;
+  field: number;
+  player: string;
+  timestamp: Date;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class ServerService {
 
-  constructor() { }
+  constructor(private fire: AngularFirestore, private router: Router) { }
+
+  async createGame() {
+    const docRef = await this.fire.collection('games').add({
+      red: (Math.random() * 100000).toString(),
+      blue: undefined,
+      status: '-'.repeat(81),
+      lastmove: undefined
+    });
+  
+    this.router.navigate(['game', docRef.id]);
+  }
 }
