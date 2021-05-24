4704771ec6bb07502a2bb925437ce62c7be122d7
Anders Kok <ak.kornerup@gmail.com>
2021-05-24 19:45:09 +0200

    Only allow place if not spectator

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -30,7 +30,9 @@ export class GameService {
   // Proxy playersReady from serverService
   public ready = this.server.playersReady;
 
-  public get canPlace(): boolean { return this.server.currentGameData?.lastplayer != this.server.uid }
+  public get canPlace(): boolean {
+    return this.server.currentGameData?.lastplayer != this.server.uid && this.server.meColor != "Spectator"
+  }
 
   constructor(
     private server: ServerService
